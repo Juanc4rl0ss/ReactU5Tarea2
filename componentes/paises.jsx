be019838d4fb509286a7322eee7ab5e93a966175
Juan Carlos <juan_c1985@hotmail.es>
2024-03-06 13:32:14 +0100

    Añadimos  en Paises un prop para cada valor del objeto data.js

--- componentes/paises.jsx
+++ componentes/paises.jsx
@@ -1,28 +1,29 @@
-import paises from '../src/data';
-
-function Paises() {
+//Componente que emplea función de props desestructurada, un prop para cada valor
+function Paises({id,destacado,imagen,nombre,precio,info}) {
   return (
     <>
-      {paises
-        .filter(pais => pais.info)
-        .map(pais => (
-          <article key={pais.id} className={`single-tour ${pais.destacado ? 'destacado' : ''}`}>
-            <img src={pais.image ? pais.image : 'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg'} alt={pais.name} />
+          {/* Aquí está la lógica para crear el componente artículo, vemos un operador ternario, para valorar si añadir o no la clase desatacado a article */}
+          <article key={id} className={`single-tour ${destacado ? 'destacado' : ''}`}>
+
+            {/* En la etiqueta imagen, incluimos un operador ternario para 'src', si hay imagen almacenada en 'data.js' la mostrará,y si no asignamos una imagen predefinida */}
+            <img src={imagen ? imagen : 'https://t4.ftcdn.net/jpg/04/70/29/97/360_F_470299797_UD0eoVMMSUbHCcNJCdv2t8B2g1GVqYgs.jpg'} alt={nombre} />
+        
             <footer>
               <div className="tour-info">
-                <h4>{pais.name}</h4>
-                
-                <h4 className="tour-price">{pais.price ? `${pais.price} €` : 'A consultar'}</h4>
+                <h4>{nombre}</h4>
+
+                {/*  En caso de que haya un precio se mostrará por pantalla,y en caso de que no lo haya, saldrá un texto predefino que diga ' A consultar,usamos otro operador ternario'*/}
+                <h4 className="tour-price">{precio ? `${precio} €` : 'A consultar'}</h4>
               </div>
-              <p>{pais.info}</p>
+              <p>{info}</p>
             </footer>
           </article>
-        ))
-      }
+      
     </>
   );
 }
 
+//Exportamos el componente Paises,para que pueda ser utilizado desde 'principal.jsx'
 export default Paises;
 
     
--- componentes/principal.jsx
+++ componentes/principal.jsx
@@ -1,7 +1,8 @@
 /**Importamos data.js, de donde obtendremos los valores para mostrar por pantalla, le damos el nombre que queramos a la
 variable,en mi caso paises**/
 
-import Paises from './paises';
+import Paises from './paises.jsx';
+import datos from '../src/data';
 
 //Empleamos la funcion App que se enlaza con main.jsx, renderizando la interfaz paises 
 function Principal() {
@@ -18,14 +19,26 @@ function Principal() {
 
            
         </section>
+        {/* Usamos map pasando por parámetro el objeto de 'data.js */}
+        {/* Lo filtramos para que solo muestre aquellos paises que contengan información */}
+        {/* Pasamos el componente 'Paises', y le asignamos valores a los props */}
+        {datos
+        .filter(dato => dato.info)
+        .map(dato => (
+        <Paises
+          key={dato.id}
+          destacado={dato.destacado}
+          imagen={dato.image}
+          nombre={dato.name}
+          precio={dato.price}
+          info={dato.info}        
+        />
 
-        <Paises />
-
-
+        ))}   
 
       </main>
     );
   }
 
-// Hacemos disponible la función App para su uso en otros componentes
+// Hacemos disponible el componente Principal para su uso desde 'main.jsx'
 export default Principal
